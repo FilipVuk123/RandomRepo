236d7c6444d4756ead30a4ae444a943864a94033
filip <fvukovic123@gmail.com>
2022-08-25 09:44:34 +0200

    Resnet model loaded in C++

---
+++ PyTorch in C++/build/CMakeFiles/example-app.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/filip/Desktop/RandomRepo/PyTorch in C++/main.cpp" "/home/filip/Desktop/RandomRepo/PyTorch in C++/build/CMakeFiles/example-app.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_C10D_GLOO"
+  "USE_DISTRIBUTED"
+  "USE_RPC"
+  "USE_TENSORPIPE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/filip/Downloads/libtorch-cxx11-abi-shared-with-deps-1.12.1+cpu/libtorch/include"
+  "/home/filip/Downloads/libtorch-cxx11-abi-shared-with-deps-1.12.1+cpu/libtorch/include/torch/csrc/api/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
